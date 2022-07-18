8d2c441b39e0b375a4ed3e13db4eaec8bc094a3d
HEE <wjdgmlckd9696@gmail.com>
2022-07-18 14:23:36 +0900

    second commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,41 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react';
 import { movieAction } from '../redux/actions/movieAction';
-import { useDispatch } from 'react-redux/es/exports';
+import { useDispatch, useSelector } from 'react-redux';
+import Banner from "../components/Banner";
+import MovieSlide from '../components/MovieSlide';
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Home = () => {
 
   const dispatch = useDispatch();
+  const { popularMovies, topRatedMovies, upComingMovies, loading } = useSelector(
+    (state) => state.movie
+    );
+  console.log("home", popularMovies)
+  
 
   useEffect( () => {
     dispatch(movieAction.getMovies());
-  },[]);
+  }, []);
+
+  // loading이 true : loading 스피너를 보여준다.
+  // loading이 false : 데이터를 보여준다.
+
+  // true : 데이터 도착 전
+  // false : 데이터 도착 후, 에러가 났을 때
+  if(loading) {
+    return <ClipLoader color="#ffff" loading={loading} size={150} />
+  }
   return (
-    <div>Home</div>
+    <div>
+      <Banner movie={popularMovies.results[0]} />
+      <h1>Popular Movie</h1>
+      <MovieSlide movies={popularMovies} />
+      <h1>Top rated Movie</h1>
+      <MovieSlide movies={topRatedMovies} />
+      <h1>Upcoming Movie</h1>
+      <MovieSlide movies={upComingMovies} />
+    </div>
   )
 }
 
--- src/redux/actions/movieAction.js
+++ src/redux/actions/movieAction.js
@@ -4,40 +4,40 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 function getMovies() {
     return async (dispatch) => {
-
-        const popularMovieApi = api.get(
-            `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
-            );
-
-        const topRatedApi = api.get(
-            `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
-            );
-
-        const upComingApi = api.get(
-            `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
-            );
-        
-        // 각각의 API 호출을 동시에 진행시키고 이 3개의 데이터가 다 올때까지만 기다린다.
-        let [popularMovies, topRatedMovies, upComingMovies] =await Promise.all([
-            popularMovieApi, 
-            topRatedApi, 
-            upComingApi
-        ]);
-        console.log(popularMovies);
-        console.log(topRatedMovies);
-        console.log(upComingMovies);
-        
-
-        // let url = `/movie/popular?api_key=<<api_key>>&language=en-US&page=1`;
-        // let response = await fetch(url);
-        // let data = await response.json();
-
-        // let url2 = `/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1`;
-        // let response = await fetch(url2);
-        // let data = await response.json();
-
-        // let url3 = `/movie/upcoming?api_key=<<api_key>>&language=en-US&page=1`;
-
+        try {
+            dispatch({type:"GET_MOVIES_REQUEST"});
+            const popularMovieApi = api.get(
+                `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+                );
+    
+            const topRatedApi = api.get(
+                `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+                );
+    
+            const upComingApi = api.get(
+                `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
+                );
+            
+            // 각각의 API 호출을 동시에 진행시키고 이 3개의 데이터가 다 올때까지만 기다린다.
+            let [popularMovies, topRatedMovies, upComingMovies] = await Promise.all([
+                popularMovieApi, 
+                topRatedApi, 
+                upComingApi
+            ]);
+    
+    
+            dispatch({
+                type: "GET_MOVIES_SUCCESS",
+                payload: { 
+                    popularMovies : popularMovies.data, 
+                    topRatedMovies : topRatedMovies.data, 
+                    upComingMovies : upComingMovies.data 
+                },
+            });
+        } catch(error) {
+            // 에러 핸들링 하는 곳
+            dispatch({type: "GET_MOVIES_FAILURE"})
+        }
     };
 }
 
